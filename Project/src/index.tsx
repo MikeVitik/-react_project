a500cbfc3506c91bd935fbde1503053c9efeeb51
MikeVitik <vitikm@gmail.com>
2024-05-22 10:46:37 +0300

    fix deploy - wrong  destination and set github origin

--- Project/src/index.tsx
+++ Project/src/index.tsx
@@ -36,7 +36,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/pomodoro">
+      <BrowserRouter basename="/react_project">
         <App />
       </BrowserRouter>
     </Provider>
